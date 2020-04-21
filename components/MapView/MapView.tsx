077f97262c57172811fe80c71dee95c288b1bb28
Obi Walker <obinnawalker@gmail.com>
2020-04-21 16:34:46 +0100

    add data to left panel on click

--- components/MapView/MapView.tsx
+++ components/MapView/MapView.tsx
@@ -5,14 +5,31 @@ import { clusterStyle, groupByCritical } from '../../utils/helpers'
 
 interface Props {
     countriesData: any;
+    getData: any
 }
 interface CountryProps {
     ID: any;
+    Country: string;
     CasesPerOneMillion: number;
     Cases: number;
+    TodayCases: number;
+    Updated: number;
+    Deaths: number;
+    TodayDeaths: number;
+    Recovered: number;
+    Active: number;
+    Critical: number;
+    DeathsPerOneMillion: number;
+    Tests: number;
+    TestsPerOneMillion: number;
+    AffectedCountries: number;
+    FortnightCases: Array<number>;
+    DropRate: number;
+    OneWeekProjection: number;
     CountryInfo: {
         Long: number;
         Lat: number;
+        Flag: string
     };
 }
 
@@ -30,6 +47,23 @@ interface PointProps {
         id: string;
         cases: number;
         casesPerMillion: number;
+        country: string;
+        todayCases: number;
+        updated: number;
+        deaths: number;
+        todayDeaths: number;
+        recovered: number;
+        active: number;
+        critical: number;
+        casesPerOneMillion: number;
+        deathsPerOneMillion: number;
+        tests: number;
+        testsPerOneMillion: number;
+        affectedCountries: number;
+        flag: string;
+        fortnightCases: Array<number>;
+        dropRate: number;
+        oneWeekProjection: number;
     };
 }
 
@@ -67,16 +101,33 @@ const MapView: React.FC<Props> = (props) => {
         }
     }, []);
 
-    const { countriesData } = props
+    const { countriesData, getData } = props
     const data: any = countriesData.length > 0 ? countriesData : [];
     const points = data.map((country: CountryProps) => ({
         type: "Feature",
         properties: {
-            cluster: false,
-            id: country.ID,
-            category: groupByCritical(country.CasesPerOneMillion),
-            cases: country.Cases,
-            casesPerMillion: country.CasesPerOneMillion
+          cluster: false,
+          id: country.ID,
+          category: groupByCritical(country.CasesPerOneMillion),
+          cases: country.Cases,
+          todayCases: country.TodayCases,
+          casesPerMillion: country.CasesPerOneMillion,
+          country: country.Country,
+          updated: country.Updated,
+          deaths: country.Deaths,
+          todayDeaths: country.TodayDeaths,
+          recovered: country.Recovered,
+          active: country.Active,
+          critical: country.Critical,
+          casesPerOneMillion: country.CasesPerOneMillion,
+          deathsPerOneMillion: country.DeathsPerOneMillion,
+          tests: country.Tests,
+          testsPerOneMillion: country.TestsPerOneMillion,
+          affectedCountries: country.AffectedCountries,
+          flag: country.CountryInfo.Flag,
+          fortnightCases: country.FortnightCases,
+          dropRate: country.DropRate,
+          oneWeekProjection: country.OneWeekProjection
         },
         geometry: {
             type: "Point",
@@ -144,22 +195,9 @@ const MapView: React.FC<Props> = (props) => {
                                   backgroundColor,
                                   fontSize: `${fontSize}em`,
                               }}
-                          // onClick={() => {
-                          //   console.log("Country>>", point.Country)
-
-                          // setViewport({
-                          //   ...viewport,
-                          //   latitude,
-                          //   longitude,
-                          //   zoom: viewport.zoom + 1,
-                          // transitionInterpolator: new FlyToInterpolator({
-                          //   speed: 2
-                          // }),
-                          // transitionDuration: "auto"
-                          //   });
-                          // }}
+                          onClick={() => getData(point.properties)}
                           >
-                              {cases}
+                            {cases}
                           </div>
                       </Marker>
                     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NextPage } from 'next';
 import fetch from 'isomorphic-unfetch';
 import dynamic from 'next/dynamic'
 import LeftPanel from '../components/LeftPanel'
+import { arraySubtraction } from '../utils/helpers'
 
 const MapView = dynamic(() => import('../components/MapView'), { ssr: false })
 
@@ -22,18 +23,44 @@ interface Props {
 		testsPerOneMillion: number,
 		affectedCountries: number
 	},
-	countries: any
+	countries: any,
+	history: any
 }
 
 const Home: NextPage<Props> = (props) => {
+
+
+	const [state, setState] = useState({
+		countryData: null
+	})
+
+	const getData = (countryData) => {
+		setState({countryData})
+	}
+
+	const handleHistory = (props) =>{
+		let history = {
+			fortnightCases : arraySubtraction(
+			Object.values(
+				props["cases"])
+				.slice(1).slice(-15)
+				.sort((a: number, b: number) => a-b
+				))
+		}
+		return history
+	} 
+
 	return (
 		<div className="flex">
 			<div className="w-1/4">
-				<LeftPanel intialCovData={props.data} />
+				<LeftPanel
+					panelData={state.countryData || {...handleHistory(props.history), ...props.data}} 
+				/>
 			</div>
 			<div className="flex-1 relative" style={{ width: '100vw', height: '100vh' }}>
 				<MapView 
-				countriesData={props.countries}
+					countriesData={props.countries}
+					getData={getData}
 				/>
 			</div>
 		</div>
@@ -43,10 +70,12 @@ const Home: NextPage<Props> = (props) => {
 Home.getInitialProps = async ({ req }) => {
 	const res = await fetch(process.env.GET_ALL_COUNTRIES);
 	const countriesData = await fetch(process.env.GET_COUNTRIES_DATA)
+	const worldHistory = await fetch(process.env.GET_WORLD_HISTORY)
 	const data = await res.json();
 	const countries = await countriesData.json()
+	const history = await worldHistory.json()
 
-	return { data, countries };
+	return { data, countries, history };
 };
 
 export default Home;
